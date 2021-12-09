bcf2d2d280310ba58da219a0ffa75e383d58be50
mbalc <mbalc909@gmail.com>
2021-12-09 01:49:45 +0100

    Solve day 6

---
+++ day/6/pt1/lanternfish.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const MAX_DAY = 80
+const MAX_TIMER = 10
+const TIMER_AFTER_RESET = 6
+const TIMER_AFTER_SPAWN = 8
+
+func main() {
+	var buffer string
+	fmt.Scanln(&buffer)
+	var splitBuffer = strings.Split(buffer, ",")
+	var timerFishCountBuckets [MAX_TIMER]int
+	for _, numString := range splitBuffer {
+		initialTimer, _ := strconv.Atoi(numString)
+		timerFishCountBuckets[initialTimer]++
+		// fmt.Println(initialTimer, timerFishCountBuckets)
+	}
+	// fmt.Println(timerFishCountBuckets)
+	for i := 0; i < MAX_DAY; i++ {
+		fishToBearChildCount := timerFishCountBuckets[0]
+		for n := 1; n < MAX_TIMER; n++ {
+			timerFishCountBuckets[n-1] = timerFishCountBuckets[n]
+		}
+		timerFishCountBuckets[TIMER_AFTER_RESET] += fishToBearChildCount
+		timerFishCountBuckets[TIMER_AFTER_SPAWN] += fishToBearChildCount
+
+		// fmt.Println(timerFishCountBuckets)
+	}
+
+	var totalFishCount = 0
+	for _, fishWithTimerTimeCount := range timerFishCountBuckets {
+		totalFishCount += fishWithTimerTimeCount
+	}
+	fmt.Println(totalFishCount)
+}
